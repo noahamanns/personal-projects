5fee978b8065da7cb595299af73a12adf759589c
namanns <namanns@sbgtv.com>
2026-03-24 16:24:07 -0700

    Update API URL to prod server

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ export const getWeatherData = async (cityName) => {
     console.log('Fetching weather for:', cityName);
 
     const response = await fetch(
-        `http://localhost:5000/api/weather?city=${cityName}`
+        `https://weather-server-production-c15e.up.railway.app/api/weather?city=${cityName}`
     );
     return response.json();
 };
