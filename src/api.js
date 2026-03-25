99bffeb303c8eff3f19cdf3418cdf5b122e0538a
namanns <namanns@sbgtv.com>
2026-03-24 18:33:02 -0700

    Update API URL to Render

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ export const getWeatherData = async (cityName) => {
     console.log('Fetching weather for:', cityName);
 
     const response = await fetch(
-        `https://weather-server-production-c15e.up.railway.app/api/weather?city=${cityName}`
+        `https://weather-server-vwqu.onrender.com/api/weather?city=${cityName}`
     );
     return response.json();
 };
