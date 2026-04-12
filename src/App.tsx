1dc4d497525f8a21ec083509d65a6a812090b694
namanns <namanns@sbgtv.com>
2026-04-11 17:25:13 -0700

    Removing Weekly

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { parse } from 'date-fns';
 
 const getGradientClass = (hour) => {
   if (hour >= 6 && hour < 9) return 'bg-sunrise';
-  if (hour >= 9 && hour < 17) return 'bg-day';
+  if (hour >= 9 && hour < 17) return 'bg-sunset';
   if (hour >= 17 && hour < 20) return 'bg-sunset';
   return 'bg-night';
 };
@@ -44,7 +44,6 @@ useEffect (() => {
       setWeatherData({
         current: {...data.current, mintemp_f, maxtemp_f},
         hourly: data.forecast.forecastday[0].hour,
-        weekly: data.forecast.forecastday.slice(1),
         location: data.location,
         forecast: data.forecast
       })
